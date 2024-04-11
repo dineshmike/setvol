5eca17166c64dfb6dface8061ddce7cc669e8525
Dinesh Subramaniam <dinesh@tcmtechs.com>
2024-04-12 01:03:34 +0800

    Changed to use csproj and replace with master volume setting functionality only

---
+++ SetVol/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SetVol;
+
+internal static partial class Program
+{
+    private static async Task Main(string[] args)
+    {
+        if (args.Length == 0 || string.IsNullOrWhiteSpace(args[0]))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Missing volume argument");
+            Console.ResetColor();
+            return;
+        }
+
+        bool isValidLevel = float.TryParse(args[0], out float level);
+        if (isValidLevel && level > 0)
+        {
+            AudioManager.SetMasterVolume(level);
+            Console.WriteLine(AudioManager.GetMasterVolume());
+        }
+    }
+}
